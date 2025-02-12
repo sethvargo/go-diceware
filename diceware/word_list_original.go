e379976b8ae09fe8138d8364e5817c9910f43718
Seth Vargo <seth@sethvargo.com>
2025-02-12 09:03:31 -0500

    Update wordlists (#14)

--- diceware/word_list_original.go
+++ diceware/word_list_original.go
@@ -1,16 +1,3 @@
-// Copyright \d{4} .*
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-// [\t\f]+|[ ]{2,}http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 package diceware
 
 import "sync"
@@ -1758,7 +1745,7 @@ var WordListOriginal = sync.OnceValue(func() WordList {
 			23121: "diem",
 			23122: "diesel",
 			23123: "diet",
-			23124: "deity",
+			23124: "diety",
 			23125: "dietz",
 			23126: "dig",
 			23131: "digit",
